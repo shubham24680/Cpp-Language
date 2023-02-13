7632913b776bdf2da01efce4b2336704c3fcdf0c
Shubham Patel <71441598+shubham24680@users.noreply.github.com>
2023-02-13 15:15:25 +0530

    modefied

--- Rough.cpp
+++ Rough.cpp
@@ -5,20 +5,13 @@ using namespace std;
 
 
 int main() {
-	int t;
-	cin >> t;
-	while(t-- > 0) {
-		int n, i, j;
-		cin n >> i >> j;
-		int ans[n][2];
+	float a = 1.333334;
+	float b = 1.4;
 
-		
-		
-
-		for(int x = i; x < n; x++){
-			for(int y = j; y < n; y++) {
-				ans[k][0] = x;
-				ans[k][1] = y;
-			}
+	if(a < b) {
+		cout << "True";
+	}
+	else {
+		cout << "False";
 	}
 }
